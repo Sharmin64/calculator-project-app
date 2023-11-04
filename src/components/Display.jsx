477da08463b31d4b20ef7ebfac9513ecc9d8cdf5
Sharmin64 <lizaeatsbanana@gmail.com>
2023-11-04 22:56:37 +0600

    display operation done

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Keypad from "./Keypad";
 
 const Display = () => {
+  const [keyClick, setKeyClick] = useState(" ");
+
+  const handleClick = (e) => {
+    setKeyClick(keyClick.concat(e.target.value));
+    console.log("I clicked the button right now!");
+  };
+  const clearScreen = () => {
+    setKeyClick("");
+  };
+  const calculate = () => {
+    setKeyClick(eval(keyClick).toString());
+  };
   return (
     <div className="container">
       <div className="main">
         <div className="screen">
-          <input type="text" className="display" placeholder="0" />
+          <input
+            value={keyClick}
+            type="text"
+            className="display"
+            placeholder="0"
+          />
         </div>
-        <Keypad />
+        <Keypad
+          handleClick={handleClick}
+          clearScreen={clearScreen}
+          calculate={calculate}
+        />
       </div>
     </div>
   );
--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,43 +1,130 @@
-import React, {useState} from "react";
-
-const Keypad = () => {
-  const [key, setKey] = useState(0);
-
-  const handleClick = (value) => {
-    setKey(value);
-    console.log("I clicked the button right now!");
-  };
+const Keypad = ({handleClick, clearScreen, calculate}) => {
   return (
     <div className="keypad">
       <div className="first-row">
-        <input type="button" value="C" className="btnA" />
-        <input type="button" value="<" className="btnA btnfold" />
-        <input type="button" value="%" className="btnA btnfold" />
-        <input type="button" value="/" className="btnA btnP" />
+        <input onClick={clearScreen} type="button" value="C" className="btnA" />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="<"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="%"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="/"
+          className="btnA btnP"
+        />
       </div>
       <div className="first-row">
-        <input type="button" value="7" className="btnA btnfold" />
-        <input type="button" value="8" className="btnA btnfold" />
-        <input type="button" value="9" className="btnA btnfold" />
-        <input type="button" value="*" className="btnA btnG" />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="7"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="8"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="9"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="*"
+          className="btnA btnG"
+        />
       </div>
       <div className="first-row">
-        <input type="button" value="4" className="btnA btnfold" />
-        <input type="button" value="5" className="btnA btnfold" />
-        <input type="button" value="6" className="btnA btnfold" />
-        <input type="button" value="-" className="btnA btnV" />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="4"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="5"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="6"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="-"
+          className="btnA btnV"
+        />
       </div>
       <div className="first-row">
-        <input type="button" value="1" className="btnA btnfold" />
-        <input type="button" value="2" className="btnA btnfold" />
-        <input type="button" value="3" className="btnA btnfold" />
-        <input type="button" value="+" className="btnA btnF" />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="1"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="2"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="3"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="+"
+          className="btnA btnF"
+        />
       </div>
       <div className="first-row">
-        <input type="button" value="0" className="btnA btnfold" />
-        <input type="button" value="00" className="btnA btnfold" />
-        <input type="button" value="." className="btnA btnfold" />
-        <input type="button" value="=" className="btnA btnY" />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="0"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="00"
+          className="btnA btnfold"
+        />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="."
+          className="btnA btnfold"
+        />
+        <input
+          onClick={calculate}
+          type="button"
+          value="="
+          className="btnA btnY"
+        />
       </div>
     </div>
   );
